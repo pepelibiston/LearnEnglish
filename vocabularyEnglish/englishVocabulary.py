0fa08d17e905503eca4a29cce0d2e8e9c94a4716
pepelibiston <andresnarvaezortiz7@gmail.com>
2022-04-26 13:53:42 +0200

    Vocabulario de sinonimos

--- vocabularyEnglish/englishVocabulary.py
+++ vocabularyEnglish/englishVocabulary.py
@@ -6,16 +6,18 @@ RANDOM = 2
 
 ALIMENTOS = 1
 ROPA = 2
-FOODACTION=3
+FOODACTION= 3
+SINONIMOS= 4
 
-vocabulariosTipo = ["alimentos", "ropa", "Acciones comida"]
-ficherosVocabulario = ["alimentos.txt", "ropa.txt", "foodAction.txt"]
+vocabulariosTipo = ["Alimentos", "Ropa", "Acciones comida", "Sinonimos"]
+ficherosVocabulario = ["alimentos.txt", "ropa.txt", "foodAction.txt","sinonimos.txt"]
 
 class Vocabulary:
     def __init__(self, vocabulario):
         self.vocabulariotxt = vocabulario
         self.espanol = []
         self.ingles = []
+        self.sinonimo = []
         self.modo = -1
 
     def procesarLinea(self, linea):
@@ -23,11 +25,22 @@ class Vocabulary:
         self.espanol.append(procesado[0])
         self.ingles.append(procesado[1])
 
-    def cargarVocabulario(self):
+    def procesarLineaS(self, linea):
+        procesado = linea.split(';')
+        self.espanol.append(procesado[0])
+        self.ingles.append(procesado[1])
+        self.sinonimo.append(procesado[2])
+
+    def cargarVocabulario(self, sinonimo):
         f = open(self.vocabulariotxt)
-        for linea in f:
-            self.procesarLinea(linea)
+        if sinonimo != 1:
+            for linea in f:
+                self.procesarLinea(linea)
+        else:
+            for linea in f:
+                self.procesarLineaS(linea)
         f.close()
+
 
     def verificar(self, pos):
         respuesta = input(">> " + self.espanol[pos] + ": ")
@@ -36,6 +49,18 @@ class Vocabulary:
             print("[+]")
         else:
             print("[-] -> " + correcto)
+
+    def verificarS(self,pos):
+        respuesta1 = input(">> " + self.espanol[pos] + ": ")
+        respuesta2 = input(">> " + self.espanol[pos] + ": ")
+        correcto1 = self.ingles[pos]
+        correcto2 = self.sinonimo[pos]
+        cond1 = str(respuesta1) == str(correcto1) or str(respuesta1) == str(correcto2)
+        cond2 = str(respuesta2) == str(correcto1) or str(respuesta2) == str(correcto2)
+        if cond1 and cond2:
+            print("[+]")
+        else:
+            print("[-] -> " + correcto1 + ", " + correcto2)
 
     def run(self):
         modo = input("Introduce modo de juego:\n\t[1] secuencial\n\t[2] random\n")
@@ -50,8 +75,22 @@ class Vocabulary:
                 x = random.randint(0,len(self.espanol)-1)
                 self.verificar(x)
 
+    def runS(self):
+        modo = input("Introduce modo de juego:\n\t[1] secuencial\n\t[2] random\n")
+        if int(modo) == int(SECUENCIAL):
+            numPalabras = len(self.espanol)
+            contador = 0
+            while contador < numPalabras:
+                self.verificarS(contador)
+                contador += 1
+        else:
+            while True:
+                x = random.randint(0,len(self.espanol)-1)
+                self.verificar(x)
+
 print("¿Que te apetece estudiar hoy?")
 contador = 1
+sinonimos = -1
 for i in vocabulariosTipo:
     print("\t[" + str(contador) + "]" + i)
     contador += 1
@@ -61,8 +100,15 @@ if int(tema) == int(ALIMENTOS):
     aptis = Vocabulary("alimentos.txt")
 elif int(tema) == int(ROPA):
     aptis = Vocabulary("ropa.txt")
-else:
+elif int(tema) == int(FOODACTION):
     aptis = Vocabulary("foodAction.txt")
+else:
+    aptis = Vocabulary("sinonimos.txt")
+    sinonimos = 1
 
-aptis.cargarVocabulario()
-aptis.run()
+if sinonimos != 1:
+    aptis.cargarVocabulario(sinonimos)
+    aptis.run()
+else:
+    aptis.cargarVocabulario(sinonimos)
+    aptis.runS()
